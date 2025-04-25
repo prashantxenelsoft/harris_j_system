5c6cfff8ab52afd08f2a67b7ce05006e3d298514
prashantxenelsoft <prashantxenelsoft@gmail.com>
2025-04-25 15:34:31 +0530

    changes

--- app/Http/Controllers/Api/ConsultancyApiController.php
+++ app/Http/Controllers/Api/ConsultancyApiController.php
@@ -13,11 +13,39 @@ class ConsultancyApiController extends Controller
     public function getConsultancy(Request $request)
     {
         $consultancies = Consultancy::orderBy('id', 'desc')->get();
-    
+
+        $countryMap = [
+            'Austria' => 'at',
+            'Afghanistan' => 'af',
+            // Add more countries as needed
+        ];
+
+        $formatted = $consultancies->map(function ($item) use ($countryMap) {
+            $country = null;
+            $flag = null;
+
+            if (preg_match('/country:\s*([^,]+)/', $item->full_address, $matches)) {
+                $country = trim($matches[1]);
+
+                $code = $countryMap[$country] ?? null;
+
+                if ($code) {
+                    $flag = "https://flagcdn.com/w80/{$code}.png"; // 80px width flag
+                }
+            }
+
+            $item->country = $country;
+            $item->flag = $flag;
+
+            return $item;
+        });
+
         return response()->json([
             'status' => true,
             'message' => 'Consultancies fetched successfully',
-            'data' => $consultancies
+            'data' => $formatted
         ], 200);
     }
+
+
 }
