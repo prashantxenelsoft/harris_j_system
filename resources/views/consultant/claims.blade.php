74d311eeb6638217dd4109e33ec9bc88caee4c79
prashantxenelsoft <prashantxenelsoft@gmail.com>
2025-05-09 16:08:00 +0530

    changes

--- resources/views/consultant/claims.blade.php
+++ resources/views/consultant/claims.blade.php
@@ -781,8 +781,8 @@
                                    const today = new Date();
                                        today.setHours(0, 0, 0, 0); // Normalize time
                            
-                                       //if (day === 0 || day === 6 || !calendarEnabled) {
-                                       if (day === 0 || day === 6 ) {
+                                       if (day === 0 || day === 6 || !calendarEnabled) {
+                                     //  if (day === 0 || day === 6 ) {
                                  
                                           cell.classList.add("disabled");
                                        } else {
--- resources/views/layouts/custom_layout.blade.php
+++ resources/views/layouts/custom_layout.blade.php
@@ -22,6 +22,10 @@
     <link rel="stylesheet" href="{{ asset('public/assets/latest/css/responsive.css') }}?v={{mt_rand(000000,999999)}}">
     <link rel="stylesheet" href="{{ asset('public/assets/latest/css/custom.css') }}?v={{mt_rand(000000,999999)}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/css/intlTelInput.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">  
 </head>
 
 <body>
