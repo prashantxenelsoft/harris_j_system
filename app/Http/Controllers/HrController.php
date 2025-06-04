5b0b81c3ee8af5883a8ef698a558fa599c1a5637
prashantxenelsoft <prashantxenelsoft@gmail.com>
2025-06-04 15:56:28 +0530

    changes

--- app/Http/Controllers/HrController.php
+++ app/Http/Controllers/HrController.php
@@ -18,7 +18,7 @@ class HrController extends Controller {
             return view('errors.404');
         }
          $consultants = DB::table('consultants')
-        ->join('clients', 'consultants.client_id', '=', 'clients.id') // if using foreign key
+        ->leftJoin('clients', 'consultants.client_id', '=', 'clients.id') // if using foreign key
         ->select(
             'consultants.*',
             'clients.serving_client as client_name'
